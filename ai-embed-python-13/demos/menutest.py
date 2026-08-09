eccca7bab32ad87b6bf0efe2f4f42e3f1c5c0047
ExMC-Github <3657936745@qq.com>
2026-08-09 08:23:39 +0800

    rm some folder

--- ai-embed-python-13/demos/menutest.py
+++
@@ -1,8 +0,0 @@
-# Run this as a python script, to gray "close" off the edit window system menu.
-import interact
-import win32con
-
-win=interact.edit.currentView.GetParent()
-menu=win.GetSystemMenu()
-id=menu.GetMenuItemID(6)
-menu.EnableMenuItem(id,win32con.MF_BYCOMMAND|win32con.MF_GRAYED)
--- ai-embed-python-13/scripts/rastest.py
+++
@@ -1,6 +0,0 @@
-import win32ras
-
-def Callback( hras, msg, state, error, exterror):
-	print "Callback called with ", hras, msg, state, error, exterror
-
-win32ras.Dial(None, None, ("Boyer",),Callback)
